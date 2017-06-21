8b922a8084b8a2ae01adb395a0b70dd1de9e30a8
gustav0naya <gea25@cornell.edu>
2017-06-21 07:37:09 -0500

    Update MVP.cs
    New commit on github

--- Models/MVP.cs
+++ Models/MVP.cs
@@ -4,7 +4,7 @@ namespace tryagain.Models
     public class MVP : NBAPlayer
     {
         public string SeasonHighlights { get; set; }
-        public string[] GamesAgainstComp { get; set; }
+        public string[] GamesAgainstOthers { get; set; }
        
         public MVP() : base()
         {
